b717240b69bda6e4f24f1267059f78ce5a6bdf93
mattinatux <berlinbullet@gmail.com>
2021-11-10 11:35:57 -0600

    event handler added for buttons and basic test function for click

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayValue = 0; // whatever is currently shown in the calc display
+
 // Function to add 2 numbers
 function add(a, b) {
     return a + b;
@@ -35,4 +37,19 @@ function operate(operator, a, b) {
     if (operator === "/") {
         return divide(a, b);
     }
+}
+
+// Select the display div and give it a starting value of 0
+const display = document.querySelector('#display');
+display.textContent = displayValue;
+
+// Select all buttons and add click event listeners
+const allButtons = document.querySelectorAll('.buttonsAll');
+allButtons.forEach( button => {
+    button.addEventListener('click', buttonClick)
+});
+
+// Function for what to do when a button is clicked
+function buttonClick() {
+    display.textContent = `nice`;
 }
