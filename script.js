8f8615e53473c466b5f36ee0306092b498bcba06
mattinatux <berlinbullet@gmail.com>
2021-11-10 11:59:49 -0600

    onClick events added to the buttons with console logging for now

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let displayValue = 0; // whatever is currently shown in the calc display
+let buttonValue; // variable to store the clicked button
+const operatorsArray = ["+", "-", "*", "/", "=", "reset", "backspace"];
 
 // Function to add 2 numbers
 function add(a, b) {
@@ -46,8 +48,24 @@ display.textContent = displayValue;
 // Select all buttons and add click event listeners
 const allButtons = document.querySelectorAll('.buttonsAll');
 allButtons.forEach( button => {
-    button.addEventListener('click', buttonClick)
+    button.addEventListener('click', buttonClick);
 });
+
+// Function to store what button was clicked
+// If button ID is an operator, then store it in operator variable
+// Else store it in display value
+// Then, in the buttonClick function, if not an operator then update the display
+
+function storeClickedButton(clicked) {
+    // if (clicked matches something from operatorsArray) {
+    //     store the id as the operator
+    // } else if (clicked matches party mode) {
+    //     have a party
+    // } else {
+    //     store the id as an update to the display value ()
+    // }
+    console.log(clicked);
+}
 
 // Function for what to do when a button is clicked
 function buttonClick() {
