9a31249214c10d4da306d211693217630caeb340
mattinatux <berlinbullet@gmail.com>
2021-11-17 12:03:35 -0600

    some refactoring, working on organizing a bit before getting more logic down

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let displayValue = 0; // whatever is currently shown in the calc display
-let buttonValue; // variable to store the clicked button
+let firstNumberStore;
+let secondNumberStore;
+let nextOperandStore; // Useful if someone wants an operator to act like an equals sign for the display
+let operandStore; // Operator to use
 const operatorsArray = ["+", "-", "*", "/", "="]; // to work with operate function
 const notMathOperatorsArray = ["reset", "backspace", "party"]; // to manipulate variable or display
 
@@ -44,57 +47,55 @@ function operate(operator, a, b) {
 
 // Select the display div and give it a starting value of 0
 const display = document.querySelector('#display');
-// display.textContent = displayValue;
+display.textContent = displayValue;
 
 // Select all buttons and add click event listeners
 const allButtons = document.querySelectorAll('.buttonsAll');
 allButtons.forEach( button => {
-    button.addEventListener('click', buttonClick());
+    button.addEventListener('click', buttonClick);
 });
 
-// Function to store what button was clicked
-// If button ID is an operator, then store it in operator variable
-// Else store it in display value
-// Then, in the buttonClick function, if not an operator then update the display
-
 function storeClickedButton(clicked) {
-    // if (clicked matches something from operatorsArray) {
-    //     store the id as the operator
-    // } else if (clicked matches notMathOperatorsArray) {
-    //     change the variable by removing last character (and update display),
-    //      or
-    //     clear the PIT variable AND anything held in answer variable
-    // } else if (clicked matches party mode) {
-    //     have a party
-    // } else {
-    //     store the id as an update to the display value ()
-    // }
-    if (display.textContent == 0) {
-        // need a int first, so if operator just return
-        if ((operatorsArray.includes(clicked) || (notMathOperatorsArray.includes(clicked)))) {
-            return;
+    // Temporary code to ignore party, backspace, and reset buttons
+    if (notMathOperatorsArray.includes(clicked)) {
+        console.log(`Currently ignoring party, backspace, and reset buttons`);
+        return;
+    }
+
+    // Equation must start with an integer
+    if ( (displayValue == 0) && ( (operatorsArray.includes(clicked)) || (notMathOperatorsArray.includes(clicked)) ) ) {
+        return;
+    }
+
+    if (operatorsArray.includes(clicked)) {
+        if (operandStore === undefined) {
+            operandStore = clicked;
+        } else {
+            nextOperandStore = clicked;
         }
-        // otherwise, put it on the screen
-        displayValue = clicked;
-        display.textContent = displayValue;
-    } else {
-        // ignore the party, backspace, and reset for now
-        if (notMathOperatorsArray.includes(clicked)) {
-            console.log(`notMathOperator clicked`)
-            return;
-        }
-        // now if we see a MATH operator, we want to start storing display values and
-        // operator in some temp variables (that I haven't declared yet!!!)
-        if (operatorsArray.includes(clicked)) {
-            console.log(`Math operator clicked`)
-            return;
-        }
+    }
+
+    // Fix to prevent leading 0 on first number entry
+    if (displayValue != 0) {
         displayValue = displayValue + clicked;
+        display.textContent = displayValue;   
+    } else {
+        displayValue = clicked;
         display.textContent = displayValue;
     }
 }
 
 // Function for what to do when a button is clicked
 function buttonClick(displayValue) {
-    display.textContent = displayValue || "0";
+    // display.textContent = displayValue || "0";
+    console.log(`click! ${this.id}`);
+
+    if (firstNumberStore === undefined) {
+        firstNumberStore = Number(displayValue);
+    } else if (secondNumberStore === undefined) {
+        secondNumberStore = Number(displayValue);
+    } else {
+        displayValue = operate(operandStore, firstNumberStore, secondNumberStore);
+        display.textContent = displayValue;
+    }
 }
